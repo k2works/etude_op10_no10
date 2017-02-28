83d1df6078a399127a346be0d28d8d9911d8958c
k2works <kakimomokuri@gmail.com>
2017-02-28 14:01:51 +0900

    開発 Gameクラス編集
    フレームごとにスコアを集計するメソッドがテストをパスするようにする。
    １回のゲームで投げられる最大数を21としてピンが倒されるごとに現在投
    げた回数をカウントアップしていく。各カウントごとに配列でスコアを保持
    してフレームごとのスコア集計に使用する。

--- app/lib/bowling/game.rb
+++ app/lib/bowling/game.rb
@@ -2,6 +2,8 @@ module Bowling
   class Game
     def initialize
       @its_score = 0
+      @its_current_throw = 0
+      @its_throws = Array.new(21,0)
     end
 
     def score
@@ -9,11 +11,21 @@ module Bowling
     end
 
     def add(pins)
+      @its_throws[@its_current_throw] = pins
+      @its_current_throw += 1
       @its_score += pins
     end
 
-    def score_for_frame(frame)
-      0
+    def score_for_frame(the_frame)
+      score = 0
+      ball = 0
+      current_frame = 0
+      while current_frame < the_frame
+        score += @its_throws[ball] + @its_throws[ball+1]
+        ball += 2
+        current_frame += 1
+      end
+      score
     end
   end
 end
