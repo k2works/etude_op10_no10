a94cc16aaefab4a75b23591fd5e9daeefbd6623f
k2works <kakimomokuri@gmail.com>
2017-02-28 18:08:58 +0900

    開発 リファクタリング
    ・メソッド抽出

--- app/lib/bowling/game.rb
+++ app/lib/bowling/game.rb
@@ -4,7 +4,7 @@ module Bowling
       @its_score = 0
       @its_current_throw = 0
       @its_throws = Array.new(21,0)
-      @first_throw = true
+      @first_throw_in_frame = true
       @its_current_frame = 1
     end
 
@@ -21,25 +21,16 @@ module Bowling
 
     def score_for_frame(the_frame)
       score = 0
-      ball = 0
+      @ball = 0
       current_frame = 0
       while current_frame < the_frame
-        first_throw = @its_throws[ball]
-        ball += 1
+        @first_throw = @its_throws[@ball]
+        @ball += 1
 
-        if first_throw == 10
-          score += 10 + @its_throws[ball] + @its_throws[ball+1]
+        if @first_throw == 10
+          score += 10 + @its_throws[@ball] + @its_throws[@ball+1]
         else
-          second_throw = @its_throws[ball]
-          ball += 1
-          frame_score = first_throw + second_throw
-
-          # スペアの得点計算には次のフレームの第１投が必要
-          if frame_score == 10
-            score += frame_score + @its_throws[ball]
-          else
-            score += frame_score
-          end
+          score += handle_second_throw
         end
 
         current_frame += 1
@@ -52,16 +43,31 @@ module Bowling
     end
 
     private
+    def handle_second_throw
+      score = 0
+      second_throw = @its_throws[@ball]
+      @ball += 1
+      frame_score = @first_throw + second_throw
+
+      # スペアの得点計算には次のフレームの第１投が必要
+      if frame_score == 10
+        score += frame_score + @its_throws[@ball]
+      else
+        score += frame_score
+      end
+      score
+    end
+
     def adjust_current_frame(pins)
-      if @first_throw
+      if @first_throw_in_frame
         if pins == 10 # ストライク
-          @first_throw = true
+          @first_throw_in_frame = true
           @its_current_frame += 1
         else
-          @first_throw = false
+          @first_throw_in_frame = false
         end
       else
-        @first_throw = true
+        @first_throw_in_frame = true
         @its_current_frame += 1
       end
       @its_current_frame = [11,@its_current_frame].min
