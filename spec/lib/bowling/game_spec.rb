d1c673575f744068e52155fbc15c210be770035f
k2works <kakimomokuri@gmail.com>
2017-02-28 13:37:51 +0900

    開発 クラス設計
    ・テストケースリファクタリング
    ・テストケース追加

--- spec/lib/bowling/game_spec.rb
+++ spec/lib/bowling/game_spec.rb
@@ -2,20 +2,36 @@ require 'rails_helper'
 include Bowling
 
 describe Bowling::Game do
-  context 'when one throw' do
-    it 'is scored' do
-      f = Game.new
-      f.add(5)
-      expect(f.score).to eq(5)
+  describe '.score' do
+    context 'when one throw' do
+      it 'is scored' do
+        g = Game.new
+        g.add(5)
+        expect(g.score).to eq(5)
+      end
+    end
+
+    context 'when two throws' do
+      it 'is scored' do
+        g = Game.new
+        g.add(5)
+        g.add(4)
+        expect(g.score).to eq(9)
+      end
     end
   end
 
-  context 'when two throws' do
-    it 'is scored' do
-      f = Game.new
-      f.add(5)
-      f.add(4)
-      expect(f.score).to eq(9)
+  describe '.score_for_frame' do
+    context 'when four throws' do
+      it 'is scored' do
+        g = Game.new
+        g.add(5)
+        g.add(4)
+        g.add(7)
+        g.add(2)
+        expect(g.score_for_frame(1)).to eq(9)
+        expect(g.score_for_frame(2)).to eq(18)
+      end
     end
   end
 end
