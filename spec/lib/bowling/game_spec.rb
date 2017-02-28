43b1f5645c6bce1e5a5918ff1a1842eabc1e6ea3
k2works <kakimomokuri@gmail.com>
2017-02-28 13:20:19 +0900

    開発 Gameクラス編集
    ・メソッド追加
    ・テストケース追加

--- spec/lib/bowling/game_spec.rb
+++ spec/lib/bowling/game_spec.rb
@@ -2,5 +2,11 @@ require 'rails_helper'
 include Bowling
 
 describe Bowling::Game do
-  it 'does something useful'
+  context 'when one throw' do
+    it 'is scored' do
+      f = Game.new
+      f.add(5)
+      expect(f.score).to eq(5)
+    end
+  end
 end
