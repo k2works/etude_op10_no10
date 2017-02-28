aa15648eff51be33bb2ee1d8b24f932e66ce8704
k2works <kakimomokuri@gmail.com>
2017-02-28 12:53:05 +0900

    開発 Frameクラス編集
    ・メソッド追加
    ・テスト追加

--- app/lib/bowling/frame.rb
+++ app/lib/bowling/frame.rb
@@ -1,7 +1,15 @@
 module Bowling
   class Frame
+    def initialize
+      @its_score = 0
+    end
+
     def get_score
-      0
+      @its_score
+    end
+
+    def add(pins)
+      @its_score = pins
     end
   end
 end
--- spec/lib/bowling/frame_spec.rb
+++ spec/lib/bowling/frame_spec.rb
@@ -7,5 +7,11 @@ describe Bowling::Frame do
       f = Frame.new
       expect(f.get_score).to eq(0)
     end
+
+    it 'is scored' do
+      f = Frame.new
+      f.add(5)
+      expect(f.get_score).to eq(5)
+    end
   end
 end
