909f2efa654f0fa4caa550c139641e96f9d6840a
k2works <kakimomokuri@gmail.com>
2017-03-03 17:38:42 +0900

    開発 ボウリングスコア計算モジュールの組み込み
    画面で入力されたスコアをコントローラでボウリングスコア計算
    モジュールに読み込ませて結果を画面に返すようにする。

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,42 @@ class HomeController < ApplicationController
   end
 
   def calculate
+    @game = Bowling::Game.new
+    @params = throw_params
+    @params.each do |k,v|
+      @game.add(v.to_i)
+    end
+    render :'home/index'
   end
 
   def clear
     redirect_to :root
+  end
+
+  private
+  def throw_params
+    params.permit(
+        :throw01,
+        :throw02,
+        :throw03,
+        :throw04,
+        :throw05,
+        :throw06,
+        :throw07,
+        :throw08,
+        :throw09,
+        :throw10,
+        :throw11,
+        :throw12,
+        :throw13,
+        :throw14,
+        :throw15,
+        :throw16,
+        :throw17,
+        :throw18,
+        :throw19,
+        :throw20,
+        :throw21
+    )
   end
 end
