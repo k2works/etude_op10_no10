712663067e9c71218e91f3a32845238fa12a3e25
k2works <kakimomokuri@gmail.com>
2017-03-04 09:53:11 +0900

    開発 UIテスト
    ・テストケースのリファクタリング
    ・パーフェクトゲームのテストケースを追加
    ・全部ガーターのテストケースを追加

--- test/system/home_test.rb
+++ test/system/home_test.rb
@@ -2,11 +2,41 @@ require 'application_system_test_case'
 
 class HomeTest < ApplicationSystemTestCase
   test "visiting the index" do
-    # トップページを開く
-    visit root_path
+    visit_root_path
 
-    # テクストが表示されているかを検証する
     assert_text 'Bowling Game Scorer'
+  end
 
+  test "calculate throw sum when perfect game" do
+    visit_root_path
+
+    set_all_same_throw('X')
+
+    click_on '計算する'
+
+    assert has_field?('throw_sum', with: '300')
+  end
+
+  test "calculate throw sum when all garter" do
+    visit_root_path
+
+    set_all_same_throw('G')
+
+    click_on '計算する'
+
+    assert has_field?('throw_sum', with: '0')
+  end
+
+  private
+  def visit_root_path
+    visit root_path
+  end
+
+  def set_all_same_throw(throw)
+    21.times do |i|
+      i+=1
+      current_throw = 'throw'+ format("%02d", i)
+      select throw, from: current_throw
+    end
   end
 end
