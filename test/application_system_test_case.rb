4abfd512f9609710570127059b7f267e1d756c0d
k2works <kakimomokuri@gmail.com>
2017-03-06 12:41:23 +0900

    構築 PhantomJS実行設定修正
    http://stackoverflow.com/questions/25673890/poltergeist-throws-js-errors-when-js-errors-false

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,6 +1,11 @@
 require "test_helper"
 require "capybara/poltergeist"
 
+options = {js_errors: false}
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, options)
+end
+
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   #driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
   driven_by :poltergeist, screen_size: [1400, 1400]
