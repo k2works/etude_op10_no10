6cb665145f9eb06aa25f38981a25f5db6ac0570a
k2works <kakimomokuri@gmail.com>
2017-04-04 12:42:36 +0900

    スコア登録メソッド追加
    ボールを投げるごとに倒すピンの数を保持する。

--- app/domains/bowling/game.rb
+++ app/domains/bowling/game.rb
@@ -1,4 +1,15 @@
 module Bowling
   class Game
+    def initialize
+      @its_score = 0
+    end
+
+    def score
+      @its_score
+    end
+
+    def add(pins)
+      @its_score = pins
+    end
   end
 end
--- spec/domains/bowling/game_spec.rb
+++ spec/domains/bowling/game_spec.rb
@@ -2,5 +2,13 @@ require 'rails_helper'
 include Bowling
 
 describe Bowling::Game do
-  it 'does something useful'
+  describe '#score' do
+    context 'when one throw' do
+      it 'is scored' do
+        f = Game.new
+        f.add(5)
+        expect(f.score).to eq(5)
+      end
+    end
+  end
 end
