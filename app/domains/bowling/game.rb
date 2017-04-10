30de302caec8c88fe284027a84581c62bf9c810b
k2works <kakimomokuri@gmail.com>
2017-04-10 10:33:42 +0900

    リファクタリング
    メソッド抽出。
    フレーム別スコア計算メソッドの第２投をプライベートメソッドに分離する。

--- app/domains/bowling/game.rb
+++ app/domains/bowling/game.rb
@@ -22,25 +22,16 @@ module Bowling
 
     def score_for_frame(the_frame)
       score = 0
-      ball = 0
+      @ball = 0
       current_frame = 0
       while current_frame < the_frame
-        first_throw = @its_throws[ball]
-        ball += 1
+        @first_throw = @its_throws[@ball]
+        @ball += 1
 
-        if first_throw == 10
-          score += 10 + @its_throws[ball] + @its_throws[ball+1]
+        if @first_throw == 10
+          score += 10 + @its_throws[@ball] + @its_throws[@ball+1]
         else
-          second_throw =  @its_throws[ball]
-          ball += 1
-          frame_score = first_throw + second_throw
-
-          # スペアの得点計算には次のフレームの第１投が必要
-          if  frame_score == 10
-            score += frame_score + @its_throws[ball]
-          else
-            score += frame_score
-          end
+          score += handle_second_throw
         end
 
         current_frame += 1
@@ -53,6 +44,21 @@ module Bowling
     end
 
     private
+    def handle_second_throw
+      score = 0
+      second_throw = @its_throws[@ball]
+      @ball += 1
+      frame_score = @first_throw + second_throw
+
+      # スペアの得点計算には次のフレームの第１投が必要
+      if frame_score == 10
+        score += frame_score + @its_throws[@ball]
+      else
+        score += frame_score
+      end
+      score
+    end
+
     def adjust_current_frame(pins)
       if @first_throw
         if pins == 10 # ストライク
