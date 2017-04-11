8c034434e23cbf9957588ddd168b08a3f0164432
k2works <kakimomokuri@gmail.com>
2017-04-11 18:54:25 +0900

    テスト８：「Add」ボタンを有効化する

--- app/javascript/packs/components.js
+++ app/javascript/packs/components.js
@@ -33,6 +33,11 @@ export class InputArea extends Component {
             text: ''
         };
         this.setText = this.setText.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.props.onSubmit(this.state.text);
     }
 
     setText(event) {
@@ -43,7 +48,7 @@ export class InputArea extends Component {
         return (
             <div>
                 <input value={this.state.text} onChange={this.setText}/>
-                <button>Add</button>
+                <button onClick={this.handleClick}>Add</button>
             </div>
         );
     }
--- test/javascript/components.spec.js
+++ test/javascript/components.spec.js
@@ -3,6 +3,7 @@ import { expect } from 'chai';
 import { shallow, mount } from 'enzyme';
 import { BeerListContainer } from '../../app/javascript/packs/components';
 import { InputArea,BeerList } from '../../app/javascript/packs/components';
+import { spy } from 'sinon';
 
 describe('BeerListContainer', () => {
     it('should render InputArea and BeerList', () => {
@@ -49,5 +50,16 @@ describe('InputArea', () => {
         input.simulate('change', {target: {value: 'Resin'}});
         expect(wrapper.state('text')).to.equal('Resin');
         expect(input.prop('value')).to.equal('Resin');
+    });
+    it('should call onSubmit when Add is clicked', () => {
+        const addItemSpy = spy();
+        const wrapper = shallow(<InputArea onSubmit={addItemSpy}/>);
+        wrapper.setState({text:'Octoberfest'});
+        const addButton = wrapper.find('button');
+
+        addButton.simulate('click');
+
+        expect(addItemSpy.calledOnce).to.equal(true);
+        expect(addItemSpy.calledWith('Octoberfest')).to.equal(true);
     });
 });
