80dc6fe69e22dd95f96ec4a5774fefbf7a67710f
k2works <kakimomokuri@gmail.com>
2017-04-11 09:19:34 +0900

    リファクタリング
    一時変数のインライン化

--- app/domains/bowling/game.rb
+++ app/domains/bowling/game.rb
@@ -25,8 +25,6 @@ module Bowling
       @ball = 0
       current_frame = 0
       while current_frame < the_frame
-        @first_throw = @its_throws[@ball]
-
         if strike
           @ball += 1
           score += 10 + next_two_balls
@@ -45,7 +43,7 @@ module Bowling
 
     private
     def strike
-      @first_throw == 10
+      @its_throws[@ball] == 10
     end
 
     def next_two_balls
@@ -54,13 +52,11 @@ module Bowling
 
     def handle_second_throw
       score = 0
-      second_throw = @its_throws[@ball+1]
-      frame_score = @first_throw + second_throw
 
       # スペアの得点計算には次のフレームの第１投が必要
       if spare
         @ball += 2
-        score += frame_score + next_ball
+        score += 10 + next_ball
       else
         score += two_balls_in_frame
         @ball += 2
