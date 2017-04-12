5006c03cfa2e00e232eb0dc4e2ba0ce7cc391401
k2works <kakimomokuri@gmail.com>
2017-04-12 15:50:36 +0900

    最初の状態

---
+++ spec/domains/video_rental/customer_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+include VideoRental
+
+describe VideoRental::Customer do
+  describe '#add_rental' do
+    it 'will do something'
+  end
+
+  describe '#statement' do
+    it 'will do something'
+  end
+end
