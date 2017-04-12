0985959c490da37c20d92c0fb8d649e754dbcca6
k2works <kakimomokuri@gmail.com>
2017-04-12 12:47:10 +0900

    ビールリストコンポーネントシステムテスト

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,18 @@
 require "test_helper"
+require "capybara/poltergeist"
+
+options = {js_errors: false,
+           timeout: 1000,
+           phantomjs_options: [
+               '--load-images=no',
+               '--ignore-ssl-errors=yes',
+               '--ssl-protocol=any']}
+
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, options)
+end
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  #driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :poltergeist, screen_size: [1400, 1400]
 end
