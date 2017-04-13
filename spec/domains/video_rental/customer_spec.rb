354b092057c98f087e3ddd67ecbc1cc762e7446d
k2works <kakimomokuri@gmail.com>
2017-04-13 12:24:29 +0900

    計算書を印刷メソッドテストケース作成

--- spec/domains/video_rental/customer_spec.rb
+++ spec/domains/video_rental/customer_spec.rb
@@ -3,10 +3,30 @@ include VideoRental
 
 describe VideoRental::Customer do
   describe '#add_rental' do
-    it 'will do something'
+    it 'should be rented' do
+      cs = Customer.new('Taro')
+      mv = VideoRental::Movie.new('Attack of the Killer Tomatoes!',Movie::REGULAR)
+      rental = VideoRental::Rental.new(mv,7)
+      cs.add_rental(rental)
+      expect(cs.instance_variable_get :@rentals).not_to be_empty
+    end
   end
 
   describe '#statement' do
-    it 'will do something'
+    it 'calculate price' do
+      cs = Customer.new('Taro')
+      mv = VideoRental::Movie.new('Attack of the Killer Tomatoes!',Movie::REGULAR)
+      rental = VideoRental::Rental.new(mv,7)
+      cs.add_rental(rental)
+      expect(cs.statement).to be_include '9.5'
+    end
+
+    it 'print statement' do
+      cs = Customer.new('Taro')
+      mv = VideoRental::Movie.new('Attack of the Killer Tomatoes!',Movie::REGULAR)
+      rental = VideoRental::Rental.new(mv,7)
+      cs.add_rental(rental)
+      expect(cs.statement).to be_match 'Rental Record for Taro\n\tAttack of the Killer Tomatoes!\t9.5\nAmount owed is 0.0\nYou earned 1 frequent renter points'
+    end
   end
 end
