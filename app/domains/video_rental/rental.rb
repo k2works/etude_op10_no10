3e9a8cacd9e7175d209b721d4f882304f8775ff2
k2works <kakimomokuri@gmail.com>
2017-04-13 14:44:34 +0900

    レンタルポイント計算メソッドの抽出
    メソッドの抽出とメソッドの移動を実施知る。

--- app/domains/video_rental/rental.rb
+++ app/domains/video_rental/rental.rb
@@ -20,5 +20,9 @@ module VideoRental
       end
       result
     end
+
+    def frequent_renter_points
+      (movie.price_code == Movie::NEW_RELEASE && days_rented > 1) ? 2 : 1
+    end
   end
 end
